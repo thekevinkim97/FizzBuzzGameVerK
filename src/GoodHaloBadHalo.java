666247ccfd2732c982d443603a72d73f231753fb
Kevin Kim <thekevinkim97@gmail.com>
2020-02-27 16:19:03 -0500

    Added input functionality in range, and trigger numbers. Can be used for different numbers.

--- src/GoodHaloBadHalo.java
+++ src/GoodHaloBadHalo.java
@@ -1,13 +1,15 @@
+import java.util.Scanner;
 
 public class GoodHaloBadHalo {
 	
-	public void halo(int max) {
+	public void halo(int max, int goodTrigger, int badTrigger) {
+		
 		for(int i = 0; i < max+1; i++) {
-			if(i % 3 == 0 && i % 5 == 0)
+			if(i % goodTrigger == 0 && i % badTrigger == 0)
 				System.out.println("GoodHalo and BadHalo");
-			else if(i % 3 == 0) 
+			else if(i % goodTrigger == 0) 
 				System.out.println("GoodHalo");
-			else if(i % 5 == 0)
+			else if(i % badTrigger == 0)
 				System.out.println("BadHalo");
 			else
 				System.out.println(i);
@@ -15,7 +17,20 @@ public class GoodHaloBadHalo {
 	}
 	
 	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		try {
+		System.out.println("What is your range, first trigger, and second trigger?");
+		int r = input.nextInt();
+		int fT = input.nextInt();
+		int sT = input.nextInt();
+		
 		GoodHaloBadHalo h = new GoodHaloBadHalo();
-		h.halo(100);
+		h.halo(r, fT, sT);
+		}
+		finally {
+			if(input != null) 
+				input.close();
+		}
 	}
 }
